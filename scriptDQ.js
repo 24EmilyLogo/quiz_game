97ab80976210ed0d442d285d00797a047144a2c7
StudentAM <studentam@Administrators-MacBook-Pro.local>
2023-11-29 10:29:14 -0800

    You'll Be in My Heart

--- scriptDQ.js
+++ scriptDQ.js
@@ -1,7 +1,23 @@
-let strtQ = document.getElementById(strtQbutton)
+//1
+let opt1 = document.getElementById(button1)
+let opt2 = document.getElementById(button2)
+let opt3 = document.getElementById(button3)
+let opt4 = document.getElementById(button4)
+let nextQ = document.getElementById(next)
+// let urAnswer = document.getElementsByClassName(buttonClass)
+//let choice = document.getElementById(choice)
 
-function startQuiz(){
-    strtQ.style.display = "none"
+//2
+function quiz(){
+    if(opt1.onclick){
+        alert("hello")
+    }
 }
 
-strtQ.addEventListener("click", startQuiz)
+nextQ.addEventListener("click", quiz)
+
+//1. variables
+//2. function
+//3. make buttons work
+    //3a. 
+//4. 
